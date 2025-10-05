cf3d16bbc7edb11426f9aa998f30c97311409cd5
hvvan9 <h.vv.an9@gmail.com>
2025-10-05 18:09:09 -0500

    Update getBondDuration.py

--- getBondDuration.py
+++ getBondDuration.py
@@ -1,33 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct  5 17:03:16 2025
+def getBondPrice(y, face, couponRate, m, ppy=1):
 
-@author: vv_an9
-"""
+    y_per = y / ppy                 # yield per period
+    n = int(m * ppy)                # total number of periods
+    c = face * couponRate / ppy     # coupon payment per period
 
-def getBondDuration(y, face, couponRate, m, ppy=1):
-    y_per = y / ppy
-    n = int(m * ppy)
-    c = face * couponRate / ppy
-
-    price = 0.0
-    weighted_pv_sum = 0.0
-
+    bondPrice = 0.0
     for t in range(1, n + 1):
+        # last period includes coupon + face
         cf = c if t < n else c + face
-        df = (1 + y_per) ** (-t)
-        pvcf = cf * df
-        price += pvcf
-        weighted_pv_sum += t * pvcf
+        df = (1 + y_per) ** (-t)    # discount factor
+        bondPrice += cf * df
 
-    macaulay_duration_years = (weighted_pv_sum / price) / ppy
-    return macaulay_duration_years
-
-y = 0.03
-face = 2_000_000
-couponRate = 0.04
-m = 10
-
-duration = getBondDuration(y, face, couponRate, m, ppy=1)
-print(f"Duration = {duration:.3f} years")
+    return bondPrice
