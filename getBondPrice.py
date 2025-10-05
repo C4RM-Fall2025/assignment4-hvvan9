f168eead1d5db207be488c28f459af2cd6d85188
hvvan9 <h.vv.an9@gmail.com>
2025-10-05 18:12:00 -0500

    Update getBondPrice.py

--- getBondPrice.py
+++ getBondPrice.py
@@ -1,23 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Oct  5 17:48:33 2025
-
-@author: vv_an9
-"""
-
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    
     y_per = y / ppy
     n = int(m * ppy)
     c = face * couponRate / ppy
 
-    price = 0.0
+    bondPrice = 0.0
     for t in range(1, n + 1):
-        # last payment includes face value
         cf = c if t < n else c + face
         df = (1 + y_per) ** (-t)
-        price += cf * df
+        bondPrice += cf * df
 
-    return price
-
+    return bondPrice
